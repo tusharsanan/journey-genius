424771bc100320fdb3c1fd7445a933ff4e0ece5e
Tushar Sanan <tushar.sanan@gmail.com>
2025-06-29 00:22:04 +0200

    refactor(app): update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,44 +1,45 @@
-import typescript from "@rollup/plugin-typescript";
-import summary from "rollup-plugin-summary";
-import terser from "@rollup/plugin-terser";
-import resolve from "@rollup/plugin-node-resolve";
-import replace from "@rollup/plugin-replace";
+/**
+ * @license
+ * Copyright (c) 2020 The Polymer Project Authors. All rights reserved.
+ * This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+ * The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+ * The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+ * Code distributed by Google as part of the polymer project is also
+ * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+ */
 
-export default {
-  input: "",
-  output: {
-    dir: "dist",
-    format: "es",
-    sourcemap: false,
-  },
-  onwarn(warning) {
-    if (warning.code !== "THIS_IS_UNDEFINED") {
-      console.error(`(!) ${warning.message}`);
-    }
-  },
-  plugins: [
-    typescript({
-      tsconfig: "./tsconfig.json",
-      sourceMap: true,
-      inlineSources: true,
-      noEmitOnError: true,
-    }),
-    replace({ preventAssignment: false, "Reflect.decorate": "undefined" }),
-    resolve(),
-    /**
-     * This minification setup serves the static site generation.
-     * For bundling and minification, check the README.md file.
-     */
-    terser({
-      ecma: 2021,
-      module: true,
-      warnings: true,
-      mangle: {
-        properties: {
-          regex: /^__/,
-        },
-      },
-    }),
-    summary(),
+import copy from "rollup-plugin-copy";
+import minifyHTML from "rollup-plugin-minify-html-literals";
+import resolve from "@rollup/plugin-node-resolve";
+
+import merge from "deepmerge";
+import { createSpaConfig } from "@open-wc/building-rollup";
+
+const baseConfig = createSpaConfig({
+  injectServiceWorker: true,
+});
+
+const copyConfig = {
+  targets: [
+    { src: "node_modules/@webcomponents", dest: "dist/node_modules" },
+    { src: "node_modules/systemjs", dest: "dist/node_modules" },
+    { src: "node_modules/lit", dest: "dist/node_modules" },
   ],
 };
+
+export default merge(baseConfig, {
+  input: "./index.html",
+  output: {
+    sourcemap: false,
+  },
+  plugins: [
+    copy(copyConfig),
+    minifyHTML(),
+    resolve({
+      customResolveOptions: {
+        moduleDirectory: "packages",
+      },
+    }),
+  ],
+  preserveEntrySignatures: false,
+});
