e7158c744024f25eacda55e27930a049b1002704
Tushar Sanan <tushar.sanan@gmail.com>
2025-06-29 14:35:27 +0200

    refactor(app): update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -11,7 +11,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import replace from "@rollup/plugin-replace";
 
 export default {
-  input: "src/journey-genius.app.ts",
+  input: "./index.html",
   output: {
     dir: "dist",
     format: "esm",
